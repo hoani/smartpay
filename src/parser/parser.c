62a8eab3a034ca50e0c104056e8f3746991185f1
hoani <hoani.bryson@gmail.com>
2020-07-08 18:23:10 +1200

    Finish off transaction parser

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -5,10 +5,12 @@
 
 static bool add_string(char * buff, size_t length, char * str);
 static bool add_start(char * buff, size_t length);
+static bool add_end(char * buff, size_t length);
 static bool add_comma(char * buff, size_t length);
 static bool add_id(int id, char * buff, size_t length);
 static bool add_card_type(CardType_t card, char * buff, size_t length);
-static bool append_card(char * buff, size_t length, bool add_comma, char * name);
+static bool add_transaction_type(TransactionType_t tt, char * buff, size_t length);
+static bool append_item(char * buff, size_t length, bool add_comma, char * name);
 
 
 bool parse_terminal(TerminalData_t terminal, char * buff, size_t length) {
@@ -24,8 +26,14 @@ bool parse_terminal(TerminalData_t terminal, char * buff, size_t length) {
   if (add_card_type(terminal.card_type, buff, length) == false) {
     return false;
   }
+  if (add_comma(buff, length) == false) {
+    return false;
+  }
+  if (add_transaction_type(terminal.transaction_type, buff, length) == false) {
+    return false;
+  }
 
-  return add_string(buff, length, ",\"TransactionType\":[\"Cheque\",\"Savings\"]}");
+  return add_end(buff, length);
 }
 
 // Helper functions
@@ -49,42 +57,66 @@ static bool add_id(int id, char * buff, size_t length) {
 }
 
 static bool add_card_type(CardType_t card, char * buff, size_t length) {
-  char card_str[64] = { '\0' };
+  char field_str[64] = { '\0' };
+  const size_t field_length = 64;
   bool include_comma = false;
 
-  if (add_string(card_str, 64, "\"cardType\":[") == false) {
+  if (add_string(field_str, field_length, "\"cardType\":[") == false) {
     return false;
   }
 
-  if (card & k_card_visa) {
-    if (append_card(card_str, 64, include_comma, "\"Visa\"") == false) {
-      return false;
+  // Iterate through all card types and check if the bit fields match.
+  // TODO: This data mappinng shouldn't really be defined here
+  const CardType_t card_bits[3] = { k_card_visa, k_card_master_card, k_card_eftpos };
+  const char names[3][16] = { "\"Visa\"", "\"MasterCard\"", "\"EFTPOS\"" };
+
+  for (size_t i = 0; i < sizeof(card_bits); i++ ) {
+    if (card & card_bits[i]) {
+      if (append_item(field_str, field_length, include_comma, (char *) names[i]) == false) {
+        return false;
+      }
+      include_comma = true;
     }
-    include_comma = true;
   }
 
-  if (card & k_card_master_card) {
-    if (append_card(card_str, 64, include_comma, "\"MasterCard\"") == false) {
-      return false;
-    }
-    include_comma = true;
-  }
-
-  if (card & k_card_eftpos) {
-    if (append_card(card_str, 64, include_comma, "\"EFTPOS\"") == false) {
-      return false;
-    }
-    include_comma = true;
-  }
-
-  if (add_string(card_str, 64, "]") == false) {
+  if (add_string(field_str, field_length, "]") == false) {
     return false;
   }
 
-  return add_string(buff, length, card_str);
+  return add_string(buff, length, field_str);
 }
 
-static bool append_card(char * buff, size_t length, bool include_comma, char * name) {
+static bool add_transaction_type(TransactionType_t tt, char * buff, size_t length) {
+  char field_str[64] = { '\0' };
+  const size_t field_length = 64;
+  bool include_comma = false;
+
+  if (add_string(field_str, field_length, "\"TransactionType\":[") == false) {
+    return false;
+  }
+
+  // Iterate through all transaction types and check if the bit fields match.
+  // TODO: This data mappinng shouldn't really be defined here
+  const TransactionType_t tt_bits[3] = { k_tt_cheque, k_tt_savings, k_tt_credit };
+  const char names[3][16] = { "\"Cheque\"", "\"Savings\"", "\"Credit\"" };
+
+  for (size_t i = 0; i < sizeof(tt_bits); i++ ) {
+    if (tt & tt_bits[i]) {
+      if (append_item(field_str, field_length, include_comma, (char *) names[i]) == false) {
+        return false;
+      }
+      include_comma = true;
+    }
+  }
+
+  if (add_string(field_str, field_length, "]") == false) {
+    return false;
+  }
+
+  return add_string(buff, length, field_str);
+}
+
+static bool append_item(char * buff, size_t length, bool include_comma, char * name) {
   if (include_comma) {
     if (add_comma(buff, length) == false) {
       return false;
--- test/tests/parserTest.cpp
+++ test/tests/parserTest.cpp
@@ -57,4 +57,101 @@ TEST_CASE( "Single Terminal Encode", "[parser]" )
 
     REQUIRE(result == expected);
   }
+
+  SECTION("JSON encode transaction types") {
+    int id = 3;
+    CardType_t cards = k_card_master_card | k_card_eftpos;
+    TransactionType_t transaction_type = k_tt_credit;
+    TerminalData_t terminal_data = { id, cards, transaction_type };
+
+    char buffer[128] = { '\0' };
+    int length = 128;
+
+    REQUIRE(parse_terminal(terminal_data, buffer, length));
+
+    std::string result(buffer);
+    std::string expected("{\"id\":3,\"cardType\":[\"MasterCard\",\"EFTPOS\"],\"TransactionType\":[\"Credit\"]}");
+
+    REQUIRE(result == expected);
+  }
+
+  SECTION("JSON buffer too small") {
+    int id = 3;
+    CardType_t cards = k_card_master_card | k_card_eftpos;
+    TransactionType_t transaction_type = k_tt_credit;
+    TerminalData_t terminal_data = { id, cards, transaction_type };
+
+    char buffer[10] = { '\0' };
+    int length = 10;
+
+    REQUIRE(parse_terminal(terminal_data, buffer, length) == false);
+  }
+
+  SECTION("JSON encode with empty cards") {
+    int id = 3;
+    CardType_t cards = 0;
+    TransactionType_t transaction_type = k_tt_credit;
+    TerminalData_t terminal_data = { id, cards, transaction_type };
+
+    char buffer[128] = { '\0' };
+    int length = 128;
+
+    REQUIRE(parse_terminal(terminal_data, buffer, length));
+
+    std::string result(buffer);
+    std::string expected("{\"id\":3,\"cardType\":[],\"TransactionType\":[\"Credit\"]}");
+
+    REQUIRE(result == expected);
+  }
+
+  SECTION("JSON encode with invalid cards") {
+    int id = 3;
+    CardType_t cards = 0xff00;
+    TransactionType_t transaction_type = k_tt_credit;
+    TerminalData_t terminal_data = { id, cards, transaction_type };
+
+    char buffer[128] = { '\0' };
+    int length = 128;
+
+    REQUIRE(parse_terminal(terminal_data, buffer, length));
+
+    std::string result(buffer);
+    std::string expected("{\"id\":3,\"cardType\":[],\"TransactionType\":[\"Credit\"]}");
+
+    REQUIRE(result == expected);
+  }
+
+  SECTION("JSON encode with empty transaction types") {
+    int id = 3;
+    CardType_t cards = k_card_master_card | k_card_eftpos;
+    TransactionType_t transaction_type = 0;
+    TerminalData_t terminal_data = { id, cards, transaction_type };
+
+    char buffer[128] = { '\0' };
+    int length = 128;
+
+    REQUIRE(parse_terminal(terminal_data, buffer, length));
+
+    std::string result(buffer);
+    std::string expected("{\"id\":3,\"cardType\":[\"MasterCard\",\"EFTPOS\"],\"TransactionType\":[]}");
+
+    REQUIRE(result == expected);
+  }
+
+  SECTION("JSON encode with invalid transaction types") {
+    int id = 3;
+    CardType_t cards = k_card_master_card | k_card_eftpos;
+    TransactionType_t transaction_type = 0xff00;
+    TerminalData_t terminal_data = { id, cards, transaction_type };
+
+    char buffer[128] = { '\0' };
+    int length = 128;
+
+    REQUIRE(parse_terminal(terminal_data, buffer, length));
+
+    std::string result(buffer);
+    std::string expected("{\"id\":3,\"cardType\":[\"MasterCard\",\"EFTPOS\"],\"TransactionType\":[]}");
+
+    REQUIRE(result == expected);
+  }
 }
